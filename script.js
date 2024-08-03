2a81ee36a0aa2930e17a89447d8d4bec3f6d578c
YassineFaidi <yassinefaidi133@gmail.com>
2024-08-03 18:08:11 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var index = 0;
+function button1_clicked() {
+    if (index == 0) {
+        window.open("https://youtu.be/DbXTBJ6_8jM?si=ZY34kjnK6wMaTcfU", '_blank');
+        index += 1
+    } else {
+        window.open("https://chatgpt.com/", '_blank');
+    }
+}
